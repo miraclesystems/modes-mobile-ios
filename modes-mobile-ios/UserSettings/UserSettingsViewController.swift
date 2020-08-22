d333235fa26302abef5e499c249eae18d1459730
Neal Gentry <neal.gentry@miraclesystems.net>
2020-08-22 06:57:59 -0400

    commit after adding

--- modes-mobile-ios/UserSettings/UserSettingsViewController.swift
+++ modes-mobile-ios/UserSettings/UserSettingsViewController.swift
@@ -7,9 +7,37 @@
 
 import UIKit
 
+protocol ChildToParentProtocol:class {
+    func btn1Clicked()
+    func locSelected()
+    func branchSelected()
+    func needToPassInfoToParent(with value:Int)
+}
+
+
 class UserSettingsViewController: UIViewController {
 
+    @IBOutlet weak var ContainerView1: UIView!
+    @IBOutlet weak var ContainerView2: UIView!
+    @IBOutlet weak var ContainerView3: UIView!
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if let vc1 = segue.destination as? Container1VC,
+                segue.identifier == "USto1" {
+                vc1.delegate = self
+            }
+            if let vc2 = segue.destination as? Container2VC,
+                segue.identifier == "USto2" {
+                vc2.delegate = self
+            }
+            if let vc3 = segue.destination as? Container3VC,
+                segue.identifier == "USto3" {
+                vc3.delegate = self
+            }
+        
+    }
+    
+   
     
     
     override func viewDidLoad() {
@@ -29,4 +57,23 @@ class UserSettingsViewController: UIViewController {
     }
     */
 
+}
+
+extension UserSettingsViewController:ChildToParentProtocol {
+
+    func btn1Clicked() {
+        print("On ParentVC")
+        ContainerView1.isHidden = true
+    }
+    func locSelected() {
+        print("On ParentVC, locSelected")
+        ContainerView2.isHidden = true
+    }
+    func branchSelected(){
+    
+    }
+    
+    func needToPassInfoToParent(with value:Int) {
+
+    }
 }
