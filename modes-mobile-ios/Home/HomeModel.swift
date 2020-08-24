9908a7cf25e1a0de254059b3c28ff94c24b712f2
Joseph Sortino <joseph.sortino@Josephs-MacBook-Pro.local>
2020-08-24 13:23:13 -0400

    added model, viewmodel templates to ios

---
+++ modes-mobile-ios/Home/HomeModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeModel.swift
+//  modes-mobile-ios
+//
+//  Created by Joseph Sortino on 8/24/20.
+//
+import Foundation
+
+
+class HomeModel : Codable{
+    
+    var value : String? = "Hello World"
+    
+}
---
+++ modes-mobile-ios/Home/HomeViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  HomeViewModel.swift
+//  modes-mobile-ios
+//
+//  Created by Joseph Sortino on 8/24/20.
+//
+
+
+import Foundation
+class HomeViewModel : NSObject, WebServiceConnectorDelegate{
+    
+    @objc dynamic var dataLoaded = false
+    var hasDataError = false
+    var dataError : String = ""
+    
+    
+    let model = HomeModel()
+    
+    func onError(_ apiError: Error) {
+        print("error")
+    }
+    
+    func onSuccess(_ jsonString: String) {
+        print("success")
+        
+       
+    }
+    
+
+    func getValue(){
+        dataLoaded = true
+    }
+    
+    
+}
+
+class HomeViewModelObserver : NSObject{
+    
+    @objc var viewModel : HomeViewModel
+       var observation: NSKeyValueObservation?
+    
+    init(object: HomeViewModel) {
+        viewModel = object
+        super.init()
+        
+        observation = observe(
+            \.viewModel.dataLoaded,
+            options: [.old, .new]
+        ) { object, change in
+            print("data changed")
+            
+            
+        }
+    }
+}
