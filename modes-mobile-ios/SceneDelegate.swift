50e9d92759a1f8a6ffeacffcd8ca6d1524890261
Neal Gentry <neal.gentry@miraclesystems.net>
2020-08-25 07:46:16 -0400

    Added Navigation folder for outline of Tab Bar Navigation with Right Side Menu with three demo viewcontrollers.

--- modes-mobile-ios/SceneDelegate.swift
+++ modes-mobile-ios/SceneDelegate.swift
@@ -30,9 +30,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         
         // this is for testing only
+        
+        //Neal - Testing: Launch Navigation
+        /*
+        let mainStoryboard = UIStoryboard(name: "Navigation", bundle: nil)
+        let VC = mainStoryboard.instantiateViewController(withIdentifier: "sidemenu_sbid") as! SSASideMenu
+        */
+        
         let mainStoryboard = UIStoryboard(name: "UserSettings", bundle: nil)
-           let VC = mainStoryboard.instantiateViewController(withIdentifier: "UserSettingsViewController") as! UserSettingsViewController
-          
+        let VC = mainStoryboard.instantiateViewController(withIdentifier: "UserSettingsViewController") as! UserSettingsViewController
+        
+                
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window!.windowScene = windowScene
