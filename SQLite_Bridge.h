77bfc93fd8f8cf8db058bcc6cc327d98d116e3fa
Joseph Sortino <joseph.sortino@Josephs-MacBook-Pro.local>
2020-08-28 11:44:50 -0400

    added sqlite, db test query called in UserSettingsViewCOntroller

---
+++ SQLite_Bridge.h
@@ -0,0 +1,14 @@
+//
+//  SQLite_Bridge.h
+//  modes-mobile-ios
+//
+//  Created by Joseph Sortino on 8/28/20.
+//
+
+#import "SQLite3.h"
+#import <time.h>
+#ifndef SQLite_Bridge_h
+#define SQLite_Bridge_h
+
+
+#endif /* SQLite_Bridge_h */
