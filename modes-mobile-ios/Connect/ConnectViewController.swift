fd11be87f114ffb9e42942fe7a8e9ecead72e257
Joseph Sortino <joseph.sortino@Josephs-MacBook-Pro.local>
2020-09-04 13:22:07 -0400

    wired up connect view controller, fixed branch picker

--- modes-mobile-ios/Connect/ConnectViewController.swift
+++ modes-mobile-ios/Connect/ConnectViewController.swift
@@ -15,17 +15,73 @@ class ConnectViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+            
+    }
+    
+    
+    @IBAction func makeCall(_ sender: Any) {
+             let phoneURL = URL(string: "tel://8003429647")!
+          UIApplication.shared.open(phoneURL, options: [:], completionHandler: nil)
+      }
 
-      
+
+    @IBAction func internationalCalling(_ sender: Any) {
+        let webUrl = URL(string: "https://www.militaryonesource.mil/international-calling-options")!
+                                   UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+        
+    }
+
+    
+    @IBAction func openWeblink(_ sender: Any) {
+        let webUrl = URL(string: "https://www.militaryonesource.mil/")!
+                 UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    @IBAction func OpenFacebook(_ sender: Any) {
+        let webUrl = URL(string: "https://www.facebook.com/military.1source")!
+                        UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+    }
+    
+
+    @IBAction func openTwitter(_ sender: Any) {
+        let webUrl = URL(string: "https://twitter.com/military1source")!
+                              UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
         
     }
     
     
     
-    @IBOutlet weak var finishButton: UIButton!
-
+  
     
-
+    @IBAction func openYoutube(_ sender: Any) {
+        let webUrl = URL(string: "https://www.youtube.com/user/Military1Source")!
+                              UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+        
+        
+    }
+    
+    
+    
+    @IBAction func openInstagram(_ sender: Any) {
+        let webUrl = URL(string: "https://www.instagram.com/military1source")!
+                            UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func openPinterest(_ sender: Any) {
+        let webUrl = URL(string: "https://www.pinterest.com/military1source/")!
+                            UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+  
+    
+    
     /*
     // MARK: - Navigation
 
